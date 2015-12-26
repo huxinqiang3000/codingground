d3be8e0a948ba0253b0161dc310175be1f652765
huxinqiang3000 <huxinqiang3000@163.com>
2015-12-26 21:34:19 +0800

    Created by Coding Ground Client

---
+++ DssStateMachine/StateType.java
@@ -0,0 +1,11 @@
+package ParkingCloud.Device.ConfigStateSyn.StateMachine;
+
+import java.util.*;
+
+public enum StateType
+{
+    WaitForRequest,
+    ProcessRequest,
+    WaitForResponse,
+    ProcessResonse
+}
